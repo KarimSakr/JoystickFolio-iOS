097f2ab080e10737440fc135e179d8be7d272178
Karim Sakr <karimsakr000@gmail.com>
2023-12-30 15:35:41 +0200

    Added search bar tp exploreviewcontroller

--- JoyStickFolio/Controllers/Core Tabs/ExploreViewController.swift
+++ JoyStickFolio/Controllers/Core Tabs/ExploreViewController.swift
@@ -8,11 +8,31 @@
 import UIKit
 
 class ExploreViewController: UIViewController {
-
+    
+    //MARK: - searchController
+    let searchController: UISearchController = {
+       let search = UISearchController(searchResultsController: nil)
+        search.obscuresBackgroundDuringPresentation = false
+        search.searchBar.placeholder = "Search..."
+        return search
+    }()
+    
+    //MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        searchController.searchResultsUpdater = self
+        navigationItem.searchController = searchController
         // Do any additional setup after loading the view.
     }
 
+}
+
+extension ExploreViewController: UISearchResultsUpdating {
+    
+    //MARK: - updateSearchResults
+    //TODO: Find a way to search on button pressed, not on character typing
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let text = searchController.searchBar.text else { return }
+           print(text)
+    }
 }
