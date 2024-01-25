52ef6f3058f3da0ae050832cf8b6c1b966705d3e
Karim Sakr <karimsakr000@gmail.com>
2024-01-25 20:32:24 +0200

    setup Firebase

---
+++ JoyStickFolio/Utils/Managers/Authentication/AuthenticationManager.swift
@@ -0,0 +1,16 @@
+//
+//  AuthenticationManager.swift
+//  JoyStickFolio
+//
+//  Created by Karim Sakr on 25/01/2024.
+//
+
+import Foundation
+import FirebaseAuth
+
+class AuthenticationManager {
+    
+    func createUser(with email: String, and password: String) {
+        
+    }
+}
