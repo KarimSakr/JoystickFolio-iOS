781ba300e28c6b23738b1cae164dcb7809414f81
Karim Sakr <karimsakr000@gmail.com>
2024-02-07 18:15:14 +0200

    Added alamofire and created network manager

---
+++ JoyStickFolio/Utils/Managers/Network/NetworkManager.swift
@@ -0,0 +1,20 @@
+//
+//  NetworkManager.swift
+//  JoyStickFolio
+//
+//  Created by Karim Sakr on 07/02/2024.
+//
+
+import Foundation
+import Alamofire
+import RxSwift
+
+final class NetworkManager {
+    
+//    func request<T: Codable>() -> Observable<T> {
+//        
+//        return Observable.create { observer in
+//            let request = AF.request
+//        }
+//    }
+}
