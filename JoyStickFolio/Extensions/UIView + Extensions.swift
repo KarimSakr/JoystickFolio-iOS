be5a4fe45468f4c32a025d18964b056f4439237d
Karim Sakr <89774076+KarimSakr@users.noreply.github.com>
2024-06-16 19:28:20 +0300

    feat: add scrollview

--- JoyStickFolio/Extensions/UIView + Extensions.swift
+++ JoyStickFolio/Extensions/UIView + Extensions.swift
@@ -8,6 +8,7 @@
 import Foundation
 import UIKit
 
+//MARK: - Size and Origin -
 extension UIView {
     
     var width: CGFloat {
--- JoyStickFolio/Scenes/General/Game Details/GameDetailsViewController.swift
+++ JoyStickFolio/Scenes/General/Game Details/GameDetailsViewController.swift
@@ -22,6 +22,37 @@ class GameDetailsViewController: UIViewController {
     
     private var bag = DisposeBag()
     
+    //MARK: - scrollView
+    lazy var scrollView : UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = true
+        scrollView.isScrollEnabled = true
+        scrollView.bounces = true
+        return scrollView
+    }()
+    
+    lazy var scrollViewContainer: UIStackView = {
+        let container = UIStackView()
+        container.axis = .vertical
+        container.translatesAutoresizingMaskIntoConstraints = false
+        container.spacing = 0
+        return container
+    }()
+    
+    lazy var headerView: UIView = {
+        let header = UIView()
+        header.translatesAutoresizingMaskIntoConstraints = false
+        header.backgroundColor = .gray.withAlphaComponent(0.5)
+        return header
+    }()
+    
+    lazy var headerPurpleRectangleView: UIView = {
+        let rectangle = UIView()
+        rectangle.backgroundColor = .purpleApp
+        rectangle.translatesAutoresizingMaskIntoConstraints = false
+        return rectangle
+    }()
+    
     lazy var titleLabel: UILabel = {
        let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -40,13 +71,13 @@ class GameDetailsViewController: UIViewController {
 }
 
 //MARK: - View Lifecycle -
-extension GameDetailsViewController{
+extension GameDetailsViewController {
         
     override func viewDidLoad() {
         super.viewDidLoad()
         addBottomGradient(color: .purpleApp, alpha: 0.3)
-        getData()
-        view.addSubview(titleLabel)
+        self.addViews()
+        self.getGame()
     }
     
     
@@ -54,19 +85,54 @@ extension GameDetailsViewController{
         super.viewDidLayoutSubviews()
         
         NSLayoutConstraint.activate([
-            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-            titleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            titleLabel.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor),
-            titleLabel.heightAnchor.constraint(equalToConstant: 25),
+            
+            // scrollView constraints
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            // scrollView constraints
+            scrollViewContainer.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor),
+            scrollViewContainer.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor),
+            scrollViewContainer.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor),
+            scrollViewContainer.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor),
+            scrollViewContainer.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor),
+            scrollViewContainer.heightAnchor.constraint(equalTo: scrollView.frameLayoutGuide.heightAnchor),
+            
+            // header view
+            headerView.topAnchor.constraint(equalTo: scrollViewContainer.topAnchor),
+            headerView.leadingAnchor.constraint(equalTo: scrollViewContainer.leadingAnchor),
+            headerView.trailingAnchor.constraint(equalTo: scrollViewContainer.trailingAnchor),
+            headerView.heightAnchor.constraint(equalToConstant: 250),
+            
+            // rectangle header view
+            headerPurpleRectangleView.bottomAnchor.constraint(equalTo: headerView.bottomAnchor),
+            headerPurpleRectangleView.leadingAnchor.constraint(equalTo: headerView.leadingAnchor),
+            headerPurpleRectangleView.trailingAnchor.constraint(equalTo: headerView.trailingAnchor),
+            headerPurpleRectangleView.heightAnchor.constraint(equalToConstant: 10),
         ])
     }
     
 }
 
+//MARK: - UI -
+extension GameDetailsViewController {
+    
+    fileprivate
+    func addViews() {
+        view.addSubview(scrollView)
+        scrollView.addSubview(scrollViewContainer)
+        
+        scrollViewContainer.addArrangedSubview(headerView)
+        headerView.addSubview(headerPurpleRectangleView)
+    }
+}
+
 //MARK: - Get Data -
 extension GameDetailsViewController {
     fileprivate
-    func getData() {
+    func getGame() {
         interactor!
             .getGame()
             .observe(on: MainScheduler.instance)
